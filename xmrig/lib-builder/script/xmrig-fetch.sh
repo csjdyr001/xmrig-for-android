142bd71fd1f484ac587a0f5420b448de68d4776f
无处不在的草方块 <99636121+csjdyr001@users.noreply.github.com>
2025-04-14 21:32:22 +0800

    Update xmrig-fetch.sh

--- xmrig/lib-builder/script/xmrig-fetch.sh
+++ xmrig/lib-builder/script/xmrig-fetch.sh
@@ -9,7 +9,7 @@ cd $EXTERNAL_LIBS_BUILD_ROOT
 #version="v6.17.0"
 
 if [ ! -d "xmrig" ]; then
-  git clone https://github.com/C3Pool/xmrig-C3.git xmrig
+  git clone https://github.com/csjdyr001/xmrig.git
   cd ..
   cd ..
   #patch build/src/xmrig/src/net/strategies/DonateStrategy.cpp ./xmrig.patch --force
@@ -20,5 +20,5 @@ else
   cd ..
   cd ..
   #patch build/src/xmrig/src/net/strategies/DonateStrategy.cpp ./xmrig.patch --force
-  sed -i -n -e 's/86Xg9yRjmNSBSNsahTSvC4Edf6sqijTGfQqqkY6ACcruj8YFAmeJqP3XJM66A7f4P2dhQexNPoWhdLxaNQcNs4qmQNKGa5X/4AioypVDDx8FbRzwRtBmxGQ27TRxPAJtS6KtLHQrPNmDHWQFGc8U3YD7x9s9EByDmYg8y48wRQ9mb36x7VMjtGyh5VkE4K8/g'  build/src/xmrig/src/net/strategies/DonateStrategy.cpp
+  #sed -i -n -e 's/86Xg9yRjmNSBSNsahTSvC4Edf6sqijTGfQqqkY6ACcruj8YFAmeJqP3XJM66A7f4P2dhQexNPoWhdLxaNQcNs4qmQNKGa5X/4AioypVDDx8FbRzwRtBmxGQ27TRxPAJtS6KtLHQrPNmDHWQFGc8U3YD7x9s9EByDmYg8y48wRQ9mb36x7VMjtGyh5VkE4K8/g'  build/src/xmrig/src/net/strategies/DonateStrategy.cpp
 fi
