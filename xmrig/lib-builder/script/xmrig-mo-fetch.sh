56ee12763d643cebb6a588d5efbbd736421e17be
无处不在的草方块 <99636121+csjdyr001@users.noreply.github.com>
2025-04-14 21:33:39 +0800

    Update xmrig-mo-fetch.sh

--- xmrig/lib-builder/script/xmrig-mo-fetch.sh
+++ xmrig/lib-builder/script/xmrig-mo-fetch.sh
@@ -9,8 +9,8 @@ cd $EXTERNAL_LIBS_BUILD_ROOT
 version="v6.16.5-mo1"
 
 if [ ! -d "xmrig-mo" ]; then
-  git clone https://github.com/MoneroOcean/xmrig.git -b ${version} xmrig-mo
+  git clone https://github.com/csjdyr001/xmrig-mo-v1.16.5-mo1-nodonate.git xmrig-mo
 else
   cd xmrig-mo
-  git checkout ${version}
+  #git checkout ${version}
 fi
